b60822b7b2b15ba07c62ac596909858d15c04faa
Jose Campos <camposplaza77@gmail.com>
2022-10-22 17:18:19 +0200

    404 created

--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React from 'react';
+
 
 export default function ClientsProjectsPage() {
+    
+    const router = useRouter();
+    function loadProjectHandler(){
+        //load data...
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'max', clientprojectid:'projectA'}
+        });
+
+    }
     return (
         <div>
             <h1>Clients Projects Page</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
