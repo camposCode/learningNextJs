58ff35fe388854d41ce25ef8d1f6aebd1c720448
Jose Campos <camposplaza77@gmail.com>
2022-10-22 17:02:50 +0200

    dinamic routes

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function ClientsPage() {
+    const clients = [
+        {id: 1, name: "Kenaz"},
+        {id: 2, name: "Jose"}
+    ];
+
+
     return (
         <div>
             <h1>Client Page</h1>
+            <ul>
+                {
+                    clients.map(client => <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    </li>)
+                }
+            </ul>
         </div>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function HomePage() {
     return (
         <div>
             <h1>The Home Page</h1>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link href="/clients">
+                        Clients
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
